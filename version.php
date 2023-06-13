9cb41516d70bd77e0cb53204e54ac962b8bdde15
Michael Gardener <mgardener@cissq.com>
2023-06-13 17:48:50 -0400

    Final activity warning

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021051700.01;
+$plugin->version   = 2021051700.02;
 $plugin->requires  = 2021051100;
 $plugin->component = 'mod_quiz';
